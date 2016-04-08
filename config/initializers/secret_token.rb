106fee9462c6a3ecf874ef48f55ef0208efde627
Aidan Zebertavage <aidanzeber@gmail.com>
2016-04-08 02:37:39 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MANIACChess::Application.config.secret_key_base = 'f4e0e54e5ad27a1e9a73f9be70801966b90880e25bd75cce40483789d439deeec6d1224d24fdc208d907640f5dddd6013cff2c62204250dbb8480fdcda375def'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MANIACChess::Application.config.session_store :cookie_store, key: '_MANIAC_Chess_session'
