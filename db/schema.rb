69cac86a7d3e4e49398065c92d528e883c77f5d0
Oleg Bulkin <o.bulkin@gmail.com>
2016-04-14 23:51:39 +0000

    Generated a Piece model, built a migration for it with appropriate constraints and indices, and ran the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411022723) do
+ActiveRecord::Schema.define(version: 20160414225123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pieces", force: :cascade do |t|
+    t.string   "type",                       null: false
+    t.string   "color",                      null: false
+    t.integer  "rank",                       null: false
+    t.integer  "file",                       null: false
+    t.integer  "game_id",                    null: false
+    t.boolean  "en_passant", default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "pieces", ["game_id", "color"], name: "index_pieces_on_game_id_and_color", using: :btree
+  add_index "pieces", ["game_id", "type"], name: "index_pieces_on_game_id_and_type", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
