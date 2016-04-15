2276b4e0222c97fcc1fd917596a8db84f53838df
Oleg Bulkin <o.bulkin@gmail.com>
2016-04-15 19:31:01 +0000

    Added a custom validation to the game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,10 @@ class Game < ActiveRecord::Base
   validates :white_player_id, presence: true
   validates :black_player_id, presence: true
   validates :state, presence: true, inclusion: {within: ["open", "white_in_check", "black_in_check", "white_in_mate", "black_in_mate", "draw"]}
+  validate :players_must_be_different
+
+  private
+  def players_must_be_different
+    errors.add("A game must have two different players") if white_player_id == black_player_id
+  end
 end
