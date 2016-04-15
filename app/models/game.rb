85a176811da7e4afbcd4b5bcac8c4145e84a768f
Oleg Bulkin <o.bulkin@gmail.com>
2016-04-15 19:12:26 +0000

    Added validations to the game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ActiveRecord::Base
+  validates :name, presence: true
+  validates :white_player_id, presence: true
+  validates :black_player_id, presence: true
+  validates :state, presence: true, inclusion: {within: ["open", "white_in_check", "black_in_check", "white_in_mate", "black_in_mate", "draw"]}
 end
