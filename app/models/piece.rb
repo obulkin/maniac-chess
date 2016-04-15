bc21e210a388b76018a98c064421055de2697ba5
Oleg Bulkin <o.bulkin@gmail.com>
2016-04-15 01:53:43 +0000

    Added validations to the Piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,7 @@
 class Piece < ActiveRecord::Base
+  validates :type, presence: true, inclusion: {within: ["Pawn", "Rook", "Bishop", "Knight", "Queen", "King"]}
+  validates :color, presence: true, inclusion: {within: ["white", "black"]}
+  validates :rank, presence: true, inclusion: {within: 1..8}
+  validates :file, presence: true, inclusion: {within: 1..8}
+  validates :game_id, presence: true
 end
