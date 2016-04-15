19859207866023e4b481798d9d4bb062bf063195
Oleg Bulkin <o.bulkin@gmail.com>
2016-04-15 18:07:40 +0000

    Created the games table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411022723) do
+ActiveRecord::Schema.define(version: 20160415024734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name",                             null: false
+    t.integer  "white_player_id",                  null: false
+    t.integer  "black_player_id",                  null: false
+    t.boolean  "white_can_castle", default: false, null: false
+    t.boolean  "black_can_castle", default: false, null: false
+    t.boolean  "white_to_move",    default: true,  null: false
+    t.string   "state",                            null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+  end
+
+  add_index "games", ["black_player_id", "state"], name: "index_games_on_black_player_id_and_state", using: :btree
+  add_index "games", ["white_player_id", "state"], name: "index_games_on_white_player_id_and_state", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
