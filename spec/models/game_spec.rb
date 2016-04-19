f0531ca6d000ef2cb96986dfe3f082b6b26aa3f5
brandoncha <brandoncha2005@gmail.com>
2016-04-19 21:12:47 +0000

    modifying the method a_correct_type_of_piece_on_square

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -49,40 +49,33 @@ RSpec.describe Game, type: :model do
       expect(game.pieces.count).to eq 32
     end
 
-    it "should successfully hook up the database relationship between pieces and games" do
-      piece = game.pieces.last
-      expect(piece.game).to eq(game)
-    end
-
     it "should be empty on the squares" do
       expect(game.pieces.where(rank: 3..6)).to eq([])
     end
 
     (1..8).each do |file|
       it "should have a white pawn at rank 2, file #{file}" do
-        @white_pawn = game.pieces.find{|piece| piece.rank == 2 && piece.file == file}
+        white_pawn = game.pieces.find{|piece| piece.rank == 2 && piece.file == file}
         expected = ["Pawn", "white"]
-        expect([@white_pawn.type, @white_pawn.color]).to eq(expected)
+        expect([white_pawn.type, white_pawn.color]).to eq(expected)
       end
 
       it "should have a black pawn at rank 7, file #{file}" do
-        @black_pawn = game.pieces.find{|piece| piece.rank == 7 && piece.file == file}
+        black_pawn = game.pieces.find{|piece| piece.rank == 7 && piece.file == file}
         expected = ["Pawn", "black"]
-        expect([@black_pawn.type, @black_pawn.color]).to eq(expected)
+        expect([black_pawn.type, black_pawn.color]).to eq(expected)
       end
     end
 
     {1 => "Rook", 2 => "Knight", 3 => "Bishop", 4 =>"Queen", 5 => "King", 6=> "Bishop", 7 => "Knight", 8 => "Rook"}.each do |file, type|  
-      it "should have a white piece at rank 1, file #{file}" do
-        @white_piece = game.pieces.find{|piece| piece.rank == 1 && piece.file == file}
-        expected = @white_piece.type
-        expect(@white_piece.type).to eq(expected)
+      it "should have a correct type of white piece at rank 1, file #{file}" do
+        white_piece = game.pieces.find{|piece| piece.rank == 1 && piece.file == file}
+        expect(white_piece.type).to eq(type)
       end
 
-      it "should have a black piece at rank 8, file #{file}" do
-        @black_piece = game.pieces.find{|piece| piece.rank == 8 && piece.file == file}
-        expected = @black_piece.type
-        expect(@black_piece.type).to eq(expected)
+      it "should have a correct type of black piece at rank 8, file #{file}" do
+        black_piece = game.pieces.find{|piece| piece.rank == 8 && piece.file == file}
+        expect(black_piece.type).to eq(type)
       end
     end  
   end
