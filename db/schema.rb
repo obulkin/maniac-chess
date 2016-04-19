576290bd83e6a4b2fe25638f3bfa154d9a5cb3f6
Oleg Bulkin <o.bulkin@gmail.com>
2016-04-19 03:25:01 +0000

    Created a migration for adding a foreign key constraint to pieces (tied to game ID) and ran it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418222613) do
+ActiveRecord::Schema.define(version: 20160419031953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,4 +65,5 @@ ActiveRecord::Schema.define(version: 20160418222613) do
 
   add_foreign_key "games", "users", column: "black_player_id"
   add_foreign_key "games", "users", column: "white_player_id"
+  add_foreign_key "pieces", "games", on_delete: :cascade
 end
