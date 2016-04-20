4cd96f943cd4b8597003bb9486ad6492237ad4ce
adriana <adrianamolello@gmail.com>
2016-04-20 17:38:23 +0000

    Created basic chess board with HTML/CSS for review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MANIACChess::Application.routes.draw do
   devise_for :users
+  get 'board', to: 'static_pages#board'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
