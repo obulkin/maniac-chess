5475a92c13572bab3d4859aae2911b01a6f83e5a
brandoncha <brandoncha2005@gmail.com>
2016-04-20 18:53:17 +0000

    modified find_by to where method and added pieces count testing

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe Game, type: :model do
 
   describe "Populate Board" do
     it "should have correct number of pieces" do
-      expect(game.pieces.count).to eq 32
+      expect(game.pieces.count).to eq (32)
     end
 
     it "should be empty on the squares" do
@@ -55,29 +55,33 @@ RSpec.describe Game, type: :model do
 
     (1..8).each do |file|
       it "should have a white pawn at rank 2, file #{file}" do
-        white_pawn = game.pieces.find_by(rank: 2 , file: file)
-        expect(white_pawn.present?).to eq(true)
-        expect([white_pawn.type, white_pawn.color]).to eq(["Pawn", "white"])
+        pieces = game.pieces.where(rank: 2 , file: file)
+        expect(pieces.present?).to eq(true)
+        expect(pieces.count).to eq(1)
+        expect([pieces.first.type, pieces.first.color]).to eq(["Pawn", "white"])
       end
 
       it "should have a black pawn at rank 7, file #{file}" do
-        black_pawn = game.pieces.find_by(rank: 7 , file: file)
-        expect(black_pawn.present?).to eq(true)
-        expect([black_pawn.type, black_pawn.color]).to eq(["Pawn", "black"])
+        pieces = game.pieces.where(rank: 7 , file: file)
+        expect(pieces.present?).to eq(true)
+        expect(pieces.count).to eq(1)
+        expect([pieces.first.type, pieces.first.color]).to eq(["Pawn", "black"])
       end
     end
 
     {1 => "Rook", 2 => "Knight", 3 => "Bishop", 4 =>"Queen", 5 => "King", 6=> "Bishop", 7 => "Knight", 8 => "Rook"}.each do |file, type|  
       it "should have a correct type #{type} of white piece at rank 1, file #{file}" do
-        white_piece = game.pieces.find_by(rank: 1 , file: file)
-        expect(white_piece.present?).to eq(true)
-        expect([white_piece.type, white_piece.color]).to eq([type, "white"])
+        pieces = game.pieces.where(rank: 1 , file: file)
+        expect(pieces.present?).to eq(true)
+        expect(pieces.count).to eq(1)
+        expect([pieces.first.type, pieces.first.color]).to eq([type, "white"])
       end
 
       it "should have a correct type #{type} of black piece at rank 8, file #{file}" do
-        black_piece = game.pieces.find_by(rank: 8 , file: file)
-        expect(black_piece.present?).to eq(true)
-        expect([black_piece.type, black_piece.color]).to eq([type, "black"])
+        pieces = game.pieces.where(rank: 8 , file: file)
+        expect(pieces.present?).to eq(true)
+        expect(pieces.count).to eq(1)
+        expect([pieces.first.type, pieces.first.color]).to eq([type, "black"])
       end
     end  
   end
