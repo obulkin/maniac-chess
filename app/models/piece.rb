af06172f34cc8614d3a04667cdd95ae6075d6a6c
Oleg Bulkin <o.bulkin@gmail.com>
2016-04-22 17:34:14 +0000

    Added a helper method to the piece model for determining whether a move from the piece's current location to a new spot is obstructed by other pieces

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -10,4 +10,78 @@ class Piece < ActiveRecord::Base
   def user
     color == "white" ? game.white_player : game.black_player
   end
+
+  protected
+  # This method only checks for obstructions along horizontal, vertical, and diagonal moves. Before calling it, 
+  # please make sure that the move provided falls into one of these categories and that an obstruction check is 
+  # actually needed (e.g. the piece isn't a knight, the move is not malformed).
+  def is_move_obstructed?(new_rank, new_file)
+    current_pieces = game.pieces
+
+    # These are assumed to be diagonal moves along a slope of 1
+    if new_rank > rank && new_file > file
+      (1..new_rank - rank).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank == rank + offset && piece.file == file + offset}
+        # Exception for an enemy piece in the spot at the end of the move
+        next if offset == new_rank - rank && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?
+      end
+      false
+    elsif new_rank < rank && new_file < file
+      (new_rank - rank..-1).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank == rank + offset && piece.file == file + offset}
+        next if offset == new_rank - rank && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?
+      end
+      false
+
+    # These are assumed to be diagonal moves along a slope of -1
+    elsif new_rank > rank && new_file < file
+      (1..new_rank - rank).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank == rank + offset && piece.file == file - offset}
+        next if offset == new_rank - rank && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?        
+      end
+      false
+    elsif new_rank < rank && new_file > file
+      (new_rank - rank..-1).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank == rank + offset && piece.file == file - offset}
+        next if offset == new_rank - rank && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?        
+      end
+      false
+
+    # Vertical moves     
+    elsif new_rank > rank 
+      (1..new_rank - rank).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank == rank + offset && piece.file == file}
+        next if offset == new_rank - rank && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?
+      end
+      false
+    elsif new_rank < rank 
+      (new_rank - rank..-1).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank == rank + offset && piece.file == file}
+        next if offset == new_rank - rank && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?        
+      end
+      false
+
+    # Horizontal moves
+    elsif new_file > file
+      (1..new_file - file).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank = rank && piece.file == file + offset}
+        next if offset == new_file - file && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?         
+      end
+      false
+    elsif new_file < file
+      (new_file - file..-1).each do |offset|
+        piece_at_offset = current_pieces.select{|piece| piece.rank = rank && piece.file == file + offset}
+        next if offset == new_file - file && piece_at_offset.present? && piece_at_offset.first.color != color 
+        return true if piece_at_offset.present?  
+      end
+      false
+    end
+  end
 end
