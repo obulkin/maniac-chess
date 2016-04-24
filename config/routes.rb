ea2996b0fff09535163f1737b649508668e48677
adriana <adrianamolello@gmail.com>
2016-04-24 23:41:29 +0000

    deleted changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 MANIACChess::Application.routes.draw do
   devise_for :users
-  get 'board', to: 'static_pages#board'
+  
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
