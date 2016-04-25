1a7b88fec9336f12acc82c499f8d5124eddcd301
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-04-25 03:41:38 +0000

    Matched /auth/failure with omniauth_callbacks failure method. Removed on_failure proc from devise.rb as it wasn't necessary.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,7 +264,4 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  OmniAuth.config.on_failure = Proc.new { |env|
-    OmniAuth::FailureEndpoint.new(env).redirect_to_failure
-  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 MANIACChess::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  # devise_scope :user do
-  #   get '/users/auth/failure' => 'sessions#failure'
-  # end
+  devise_scope :user do
+    get '/users/auth/failure' => 'omniauth_callbacks#failure'
+  end
   root 'static_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
