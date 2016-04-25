c65280ee6fed169074fefdebdfb6127111cfb87b
Aidan Zebertavage <aidanzeber@gmail.com>
2016-04-25 23:27:24 +0000

    removed associated rspec testing for index function

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
-  describe "games#index action" do
-    it "should successfully show the page" do 
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "games#new action" do 
     it "should sucessfully show the new form" do
       get :new
