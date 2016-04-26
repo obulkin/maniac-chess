d9d9bae37985cfc89708062392496576c2f64751
Aidan Zebertavage <aidanzeber@gmail.com>
2016-04-26 00:58:26 +0000

    fixed syntax issue in RSPEC

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe GamesController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+  end
 
   describe "games#show action" do 
     it "should successfully show the page if the game is found" do
