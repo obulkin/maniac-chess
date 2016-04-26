376296fad76af7a9f590af2d128ac94c20a61653
Aidan Zebertavage <aidanzeber@gmail.com>
2016-04-26 01:04:07 +0000

    updated test descriptions

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
 
   describe "games#new action" do 
-    it "should sucessfully show the new form" do
+    it "should return success response on the get request" do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "games#show action" do 
-    it "should successfully show the page if the game is found" do
+    it "should return success response on the get request" do
       game = create(:game)
       get :show, id: game.id
       expect(response).to have_http_status(:success)
