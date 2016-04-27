0250938abd5cafb536adf132e2867579b43a3c6e
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-04-27 04:50:12 +0000

    Updated omniauth callbacks controller spec and deleted request spec.

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -2,87 +2,79 @@ require 'rails_helper'
 
 RSpec.describe OmniauthCallbacksController, :type => :controller do
 
-  describe "#facebook callback" do
+  describe "#facebook" do
 
-    context "given user did persist" do
+    context "given user does persist" do
       before do
-        OmniAuth.config.mock_auth[:do_omniauth] = OmniAuth::AuthHash.new({
-          :provider => 'facebook',
-          :uid => '123545',
-          :info => { :email => 'testuser@facebook.com' }
-          })
+        OmniAuth.config.mock_auth[:do_omniauth] = FactoryGirl.create(:auth_hash, :facebook)
 
         request.env["devise.mapping"] = Devise.mappings[:user]
         request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:do_omniauth]
-        get :facebook
       end
 
-      it "should successfully authenticate from user's Facebook account" do
+      it "should successfully authenticate using Facebook authentication hash and display a success message in flash" do
+        get :facebook
         user = User.find_by(email: 'testuser@facebook.com')
         expect(user.id).not_to eq(nil)
         expect(flash[:notice]).to eq("Successfully authenticated from Facebook account.")
-        # expect(response).to redirect_to root_path
       end
     end
 
-    context "given user did not persist" do
+    context "given user does not persist" do
       before do
-        OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-          :provider => 'facebook',
-          :uid => '54321',
-          :info => { :email => '' }
-          })
+        OmniAuth.config.mock_auth[:do_omniauth] = FactoryGirl.create(:auth_hash, :facebook, :does_not_persist)
 
         request.env["devise.mapping"] = Devise.mappings[:user]
-        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-        get :facebook
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:do_omniauth]
       end
 
-      it "should redirect user to new_user_registration_url" do
+      it "should redirect the user to the new user registration form" do
+        get :facebook
         expect(response).to redirect_to new_user_registration_url
       end
     end
   end
 
 
-  describe "#google_oauth2 callback" do
+  describe "#google_oauth2" do
 
-    context "given user did persist" do
+    context "given user does persist" do
       before do
-        OmniAuth.config.mock_auth[:do_omniauth] = OmniAuth::AuthHash.new({
-          :provider => 'google_oauth2',
-          :uid => '678910',
-          :info => { :email => 'testuser@gmail.com' }
-          })
+        OmniAuth.config.mock_auth[:do_omniauth] = FactoryGirl.create(:auth_hash, :google_oauth2)
 
         request.env["devise.mapping"] = Devise.mappings[:user]
         request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:do_omniauth]
-        get :google_oauth2
       end
 
-      it "should successfully authenticate from user's Google account and set flash notice" do
+      it "should successfully authenticate using Google authentication hash and display a success message in flash" do
+        get :google_oauth2
         user = User.find_by(email: 'testuser@gmail.com')
         expect(user.id).not_to eq(nil)
         expect(flash[:notice]).to eq("Successfully authenticated from Google account.")
       end
     end
 
-    context "given user did not persist" do
+    context "given user does not persist" do
       before do
-        OmniAuth.config.mock_auth[:do_omniauth] = OmniAuth::AuthHash.new({
-          :provider => 'google_oauth2',
-          :uid => '019876',
-          :info => { :email => '' }
-          })
+        OmniAuth.config.mock_auth[:do_omniauth] = FactoryGirl.create(:auth_hash, :google_oauth2, :does_not_persist)
 
         request.env["devise.mapping"] = Devise.mappings[:user]
         request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:do_omniauth]
-        get :google_oauth2
       end
 
-      it "should redirect user to new_user_registration_url" do
+      it "should redirect the user to the new user registration form" do
+        get :google_oauth2
         expect(response).to redirect_to new_user_registration_url
       end
+    end
+  end
+
+  describe "#failure" do
+    it "should redirect to the homepage and add an error message to the flash" do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      get :failure
+      expect(response).to redirect_to root_path
+      expect(flash[:alert]).to eq("Could not authenticate.")
     end
   end
 end
