bc8b9692de612a7bd1b0ae8197f41ea2c1da0e55
Aidan Zebertavage <aidanzeber@gmail.com>
2016-04-29 00:43:27 +0000

    added user sign in to the create tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -27,12 +27,16 @@ RSpec.describe GamesController, type: :controller do
   describe "games#create action" do
     context "with valid attributes" do
       it "creates a new game" do 
+        user = create(:user)
+        sign_in user
         expect{
           post :create, game: FactoryGirl.attributes_for(:game)
         }.to change(Game,:count).by(1)
       end
 
       it "redirects to the new game" do
+        user = create(:user)
+        sign_in user
         post :create, game: FactoryGirl.attributes_for(:game)
         expect(response).to redirect_to Game.last
       end
@@ -40,12 +44,16 @@ RSpec.describe GamesController, type: :controller do
 
     context "with invalid attributes" do 
       it "does not save the new game" do
+        user = create(:user)
+        sign_in user
         expect{
           post :create, game: FactoryGirl.attributes_for(:invalid_game)
         }.to_not change(Game, :count)
       end
 
       it "re-renders the new method" do
+        user = create(:user)
+        sign_in user
         post :create, game: FactoryGirl.attributes_for(:invalid_game)
         expect(response).to render_template :new
       end
