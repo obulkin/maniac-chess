1bdd288b6789e49269537590bc6664d85cec8975
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-04-30 04:44:04 +0000

    Removed provider name :google_oauth2 and replaced with :google.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:google_oauth2, :facebook]
+         :omniauthable, :omniauth_providers => [:google, :facebook]
 
   def games
     Game.where "white_player_id = :user_id OR black_player_id = :user_id", {user_id: id}
