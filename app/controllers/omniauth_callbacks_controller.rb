0186096282458c1df384225c1fae0612d79a61aa
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-04-30 04:46:20 +0000

    Removed code that replaced "google_oauth2" with "Google" when printing provider kind. Removed session["devise.provider_data] as it wasn't needed. Updated flash messages.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,24 +5,20 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     provider_data = @user.provider
     provider_kind = @user.provider.capitalize
 
-    if @user.provider == "google_oauth2"
-      provider_kind = "Google"
-    end
-
     if @user.persisted?
       flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: "#{provider_kind}")
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.#{provider_data}_data"] = request.env["omniauth.auth"]
+      flash[:notice] = "We were not able to authenticate you. Try signing up directly with MANIAC chess by entering an email address and creating a new password."
       redirect_to new_user_registration_url
     end
   end
 
   alias_method :facebook, :do_omniauth
-  alias_method :google_oauth2, :do_omniauth
+  alias_method :google, :do_omniauth
 
   def failure
-    flash[:alert] = "Could not authenticate."
+    flash[:alert] = "Uh oh, looks like something went wrong! Please try again later or sign up with MANIAC Chess directly by providing an email address and password."
     redirect_to root_path
   end
 end
