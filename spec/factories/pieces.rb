985bb81a8089de381c97947b7f9c34e4416d9574
brandoncha <brandoncha2005@gmail.com>
2016-05-05 23:23:05 +0000

    modified valid king move

--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -6,12 +6,4 @@ FactoryGirl.define do
     file 1
     association :game
   end
-
-  factory :king do
-    type "King"
-    color "white"
-    rank 1
-    file 5
-    association :game
-  end
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
-	let(:king) {create :king}
-
+  
   describe "King's valid_move" do
   	it "should be inside destination, blocked" do
+      game = create :game
+      king = game.pieces.find_by(rank: 1, file: 5, color: "white")
+      king2 = game.pieces.find_by(rank: 8, file: 5, color: "black")
   		expect(king.valid_move?(1, 6)).to eq(false)
   		expect(king.valid_move?(1, 4)).to eq(false)
   		expect(king.valid_move?(2, 5)).to eq(false)
+      expect(king.valid_move?(2, 4)).to eq(false)
+      expect(king.valid_move?(2, 6)).to eq(false)
+      expect(king2.valid_move?(7, 5)).to eq(false)
+      expect(king2.valid_move?(7, 4)).to eq(false)
+      expect(king2.valid_move?(7, 6)).to eq(false)
   	end
 
     it "shouldn't be outside destination" do
-      expect(king.valid_move?(1, 8)).to eq(false)
-      expect(king.valid_move?(1, 2)).to eq(false)
-      expect(king.valid_move?(3, 8)).to eq(false)
+      game = create :game
+      king1 = game.pieces.create(type: "King", rank: 3, file: 6, color: "white")
+      king2 = game.pieces.create(type: "King", rank: 6, file: 3, color: "black")
+      expect(king1.valid_move?(5, 6)).to eq(false)
+      expect(king1.valid_move?(3, 4)).to eq(false)
+      expect(king1.valid_move?(3, 8)).to eq(false)
+      expect(king1.valid_move?(5, 4)).to eq(false)
+      expect(king1.valid_move?(5, 8)).to eq(false)
+      expect(king2.valid_move?(4, 3)).to eq(false)
+      expect(king2.valid_move?(4, 1)).to eq(false)
+      expect(king2.valid_move?(4, 5)).to eq(false)
     end
 
   	it "should be inside destination, not blocked" do
@@ -21,7 +36,19 @@ RSpec.describe King, type: :model do
   		king2 = game.pieces.create(type: "King", rank: 4,  file: 5, color: "black")
   		expect(king2.valid_move?(4, 6)).to eq(true)
   		expect(king2.valid_move?(4, 4)).to eq(true)
+      expect(king2.valid_move?(5, 5)).to eq(true)
+      expect(king2.valid_move?(3, 5)).to eq(true)
+      expect(king2.valid_move?(5, 4)).to eq(true)
   		expect(king2.valid_move?(5, 6)).to eq(true)
+      expect(king2.valid_move?(3, 4)).to eq(true)
+      expect(king2.valid_move?(3, 6)).to eq(true)
   	end
+
+    it "shouldn't be on same square after moving" do
+      game = create :game
+      king = game.pieces.find_by(rank: 1, file: 5, color: "white")
+      expect(king.valid_move?(1, 5)).to eq(false)
+    end
+
   end
 end
