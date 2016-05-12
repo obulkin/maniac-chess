65fa55a2fc99d10dd72156324025aff6642636d5
Oleg Bulkin <o.bulkin@gmail.com>
2016-05-12 19:55:51 +0000

    Updated Knight#valid_move? to better align with common Ruby style rules

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,12 +3,12 @@ class Knight < Piece
     rank_diff = new_rank - rank
     file_diff = new_file - file
 
-    return false if is_move_malformed?(new_rank, new_file)
+    return false if is_move_malformed? new_rank, new_file
 
     # Negation of standard knight moves
     return false if !((rank_diff.abs == 1 && file_diff.abs == 2) || (rank_diff.abs == 2 && file_diff.abs == 1))
     # Obstruction at move destination
-    return false if game.pieces.find_by(rank: new_rank, file: new_file, color: color)
+    return false if game.pieces.find_by rank: new_rank, file: new_file, color: color
 
     true
   end
