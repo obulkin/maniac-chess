0a209ba4014f9023d498716b6acb0680d046c9f5
Oleg Bulkin <o.bulkin@gmail.com>
2016-05-12 18:55:40 +0000

    Created Knight#valid_move?, which accepts a rank and a file and determines whether a move to that spot would be valid

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,15 @@
 class Knight < Piece
+  def valid_move?(new_rank, new_file)
+    rank_diff = new_rank - rank
+    file_diff = new_file - file
+
+    return false if is_move_malformed?(new_rank, new_file)
+
+    # Negation of standard knight moves
+    return false if !((rank_diff.abs == 1 && file_diff.abs == 2) || (rank_diff.abs == 2 && file_diff.abs == 1))
+    # Obstruction at move destination
+    return false if game.pieces.find_by(rank: new_rank, file: new_file, color: color)
+
+    true
+  end
 end
