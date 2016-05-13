4df15456550a067d3b80ed86fd748cd6cb1ba449
brandoncha <brandoncha2005@gmail.com>
2016-05-13 20:02:18 +0000

    valid_move? for Rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,11 @@
 class Rook < Piece
+    def valid_move?(new_rank, new_file)
+      if !is_move_malformed?(new_rank, new_file) && (horizontal_move?(new_rank, new_file) || vertical_move?(new_rank, new_file)) && !is_move_obstructed?(new_rank, new_file)
+        true
+      else
+        false
+    end
+  end
 end
+
+
---
+++ spec/models/rook_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe Rook, type: :model do
+  describe "#valid_move?" do
+    let(:game) {create :game}
+
+    it "should properly evaluate malformed moves that are otherwise valid" do
+      white_rook = game.pieces.find_by(rank: 1, file: 1)
+      black_rook = game.pieces.find_by(rank: 8, file: 1)
+      expect(white_rook.valid_move?(0, 1)).to eq(false)
+      expect(white_rook.valid_move?(1, 0)).to eq(false)
+      expect(white_rook.valid_move?(0, 0)).to eq(false)
+      expect(white_rook.valid_move?(0, 2)).to eq(false)
+      expect(white_rook.valid_move?(2, 0)).to eq(false)
+      expect(white_rook.valid_move?(1, 1)).to eq(false)
+      expect(black_rook.valid_move?(8, 1)).to eq(false)
+      expect(black_rook.valid_move?(7, 0)).to eq(false)
+      expect(black_rook.valid_move?(8, 0)).to eq(false)
+      expect(black_rook.valid_move?(9, 0)).to eq(false)
+      expect(black_rook.valid_move?(9, 1)).to eq(false)
+      expect(black_rook.valid_move?(9, 2)).to eq(false)
+    end
+
+    it "should properly evaluate obstructed moves that are otherwise valid" do
+      white_rook = game.pieces.find_by(rank: 1, file: 1)
+      black_rook = game.pieces.find_by(rank: 8, file: 1)
+      expect(white_rook.valid_move?(1, 2)).to eq(false)
+      expect(white_rook.valid_move?(2, 1)).to eq(false)
+      expect(black_rook.valid_move?(8, 2)).to eq(false)
+      expect(black_rook.valid_move?(7, 1)).to eq(false)
+    end
+
+    it "should properly evaluate unobstructed moves that are not allowed for a Rook but are otherwise valid" do
+      white_rook = game.pieces.create(type: "Rook", rank: 4, file: 4, color: "white")
+      expect(white_rook.valid_move?(3, 5)).to eq(false)
+      expect(white_rook.valid_move?(5, 5)).to eq(false)
+      expect(white_rook.valid_move?(3, 3)).to eq(false)
+      expect(white_rook.valid_move?(3, 6)).to eq(false)
+    end
+
+    it "should properly evaluate valid moves" do
+      white_rook = game.pieces.create(type: "Rook", rank: 4, file: 4, color: "white")
+      game.pieces.where(rank: 2).destroy_all
+      expect(white_rook.valid_move?(4, 3)).to eq(true)
+      expect(white_rook.valid_move?(4, 2)).to eq(true)
+      expect(white_rook.valid_move?(4, 5)).to eq(true)
+      expect(white_rook.valid_move?(4, 6)).to eq(true)
+      expect(white_rook.valid_move?(3, 4)).to eq(true)
+      expect(white_rook.valid_move?(2, 4)).to eq(true) 
+      expect(white_rook.valid_move?(5, 4)).to eq(true)
+      expect(white_rook.valid_move?(6, 4)).to eq(true)   
+    end
+  end
+end
