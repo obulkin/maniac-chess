43602775f798df1caefeb6f6d71cf07eb41da2ad
brandoncha <brandoncha2005@gmail.com>
2016-05-14 20:17:15 +0000

    modfied identation on valid_move for rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,9 +1,9 @@
 class Rook < Piece
-    def valid_move?(new_rank, new_file)
-      if !is_move_malformed?(new_rank, new_file) && (rank == new_rank || file == new_file) && !is_move_obstructed?(new_rank, new_file)
-        true
-      else
-        false
+  def valid_move?(new_rank, new_file)
+    if !is_move_malformed?(new_rank, new_file) && (rank == new_rank || file == new_file) && !is_move_obstructed?(new_rank, new_file)
+      true
+    else
+      false
     end
   end
 end
