576e0187d961b0e4b0647811b43cb523e62fffa9
brandoncha <brandoncha2005@gmail.com>
2016-05-14 00:12:42 +0000

    modified unit tests for Rook#valid_move?

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,6 +1,6 @@
 class Rook < Piece
     def valid_move?(new_rank, new_file)
-      if !is_move_malformed?(new_rank, new_file) && (horizontal_move?(new_rank, new_file) || vertical_move?(new_rank, new_file)) && !is_move_obstructed?(new_rank, new_file)
+      if !is_move_malformed?(new_rank, new_file) && (rank == new_rank || file == new_file) && !is_move_obstructed?(new_rank, new_file)
         true
       else
         false
