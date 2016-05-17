1dd95bf24c08657ff333b5c7934154b1eb3f204f
Aidan Zebertavage <aidanzeber@gmail.com>
2016-05-17 22:48:58 +0000

    fixing merge conflicts in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 MANIACChess::Application.routes.draw do
-  
+
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 
   resources :games, :only => [:new, :show, :create, :update]
-  root "games#index"
 
   root 'static_pages#index'
   resource :board, only: :show
