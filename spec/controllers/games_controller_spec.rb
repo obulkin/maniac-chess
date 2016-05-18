9d6c1ecfa7d4891251f98c169ff6397e0fd42a27
Oleg Bulkin <o.bulkin@gmail.com>
2016-05-18 00:42:02 +0000

    Updated the unit tests for GamesController#create to address a couple issues

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -31,19 +31,18 @@ RSpec.describe GamesController, type: :controller do
 
   describe "games#create action" do
     context "with valid attributes" do
-      it "creates a new game" do 
-        user = create(:user)
+      it "creates a new game using the data provided to the controller" do 
+        user = create :user
         sign_in user
-        expect{
-          post :create, game: attributes_for(:game)
-        }.to change(Game,:count).by(1)
+        expect{post :create, game: {name: "Valid Game"}}.to change(Game, :count).by(1)
+        expect(Game.find_by(name: "Valid Game")).to_not eq(nil)
       end
 
       it "redirects to the new game" do
         user = create(:user)
         sign_in user
-        post :create, game: FactoryGirl.attributes_for(:game)
-        expect(response).to redirect_to Game.last
+        post :create, game: {name: "Valid Game 2"}
+        expect(response).to redirect_to(game_path(Game.find_by(name: "Valid Game 2")))
       end
     end
 
