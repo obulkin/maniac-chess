d6df6d9bb25ff0ef6386ba1e54a16b902427d4bf
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-05-21 04:59:49 +0000

    Added valid_move? to queen model

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,7 @@
 class Queen < Piece
+  def valid_move?(new_rank, new_file)
+    return false if is_move_malformed?(new_rank, new_file) || is_move_obstructed?(new_rank, new_file)
+    
+    rank == new_rank || file == new_file || (rank - new_rank).abs == (file - new_file).abs
+  end
 end
